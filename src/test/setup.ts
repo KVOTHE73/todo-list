b18ef31d1a074fd80f31cf1ab6a3bd98bb1bbcdc
KVOTHE73 <syn231000@hotmail.com>
2025-06-25 16:37:11 +0200

    ci: run tests with vitest run

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -3,34 +3,19 @@ import { TextEncoder, TextDecoder } from "util";
 
 declare global {
   var crypto: Crypto;
+  var TextEncoder: { new (): TextEncoder; prototype: TextEncoder };
+  var TextDecoder: {
+    new (label?: string, options?: TextDecoderOptions): TextDecoder;
+    prototype: TextDecoder;
+  };
 }
 
-// Polyfill globalThis.crypto for both Vite plugin and tests
+// Polyfill WebCrypto API and esbuild invariants in tests
 Object.defineProperty(globalThis, "crypto", {
   value: webcrypto,
   configurable: true,
-  writable: false,
 });
-
-// Polyfill crypto.hash used by @vitejs/plugin-vue
-(globalThis.crypto as any).hash = async (
-  algorithm: string,
-  data: Buffer | ArrayBuffer | string
-) => {
-  let buffer: ArrayBuffer;
-  if (typeof data === "string") {
-    buffer = new TextEncoder().encode(data);
-  } else if (Buffer.isBuffer(data)) {
-    buffer = data.buffer.slice(
-      data.byteOffset,
-      data.byteOffset + data.byteLength
-    );
-  } else {
-    buffer = data;
-  }
-  return await webcrypto.subtle.digest(algorithm, buffer);
-};
-
-// Polyfill TextEncoder/TextDecoder for esbuild invariant
+(globalThis.crypto as any).hash = (...args: any[]) =>
+  (webcrypto.subtle as any).digest(...args);
 Object.defineProperty(globalThis, "TextEncoder", { value: TextEncoder });
 Object.defineProperty(globalThis, "TextDecoder", { value: TextDecoder });
