5a1efee99ea95456806f3edca24151a3c4258175
KVOTHE73 <syn231000@hotmail.com>
2025-06-25 17:32:51 +0200

    ci: run tests with vitest run

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,35 @@
+import { webcrypto } from "crypto";
+import { TextEncoder, TextDecoder } from "util";
+
+// Polyfill WebCrypto API for vite-plugin-vue getHash
+Object.defineProperty(globalThis, "crypto", {
+  value: webcrypto as any,
+  configurable: true,
+  writable: true,
+});
+(globalThis as any).crypto.hash = async function (
+  algorithm: string,
+  data: Buffer | ArrayBuffer | string
+): Promise<ArrayBuffer> {
+  let buffer: ArrayBuffer;
+  if (typeof data === "string") {
+    const uint8 = new TextEncoder().encode(data);
+    buffer = uint8.buffer;
+  } else if (Buffer.isBuffer(data)) {
+    buffer = data.buffer.slice(
+      data.byteOffset,
+      data.byteOffset + data.byteLength
+    );
+  } else {
+    buffer = data;
+  }
+  return webcrypto.subtle.digest(algorithm, buffer);
+};
+
+// Polyfill TextEncoder/TextDecoder for esbuild invariant
+Object.defineProperty(globalThis, "TextEncoder", { value: TextEncoder });
+Object.defineProperty(globalThis, "TextDecoder", { value: TextDecoder });
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
